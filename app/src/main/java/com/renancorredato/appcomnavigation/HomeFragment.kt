04d44c35a519fbfb6855964b9907292f733a3c76
Renan Corredato <renancorredato@hotmail.com>
2023-02-03 15:10:26 -0300

    insert findNavController

--- app/src/main/java/com/renancorredato/appcomnavigation/HomeFragment.kt
+++ app/src/main/java/com/renancorredato/appcomnavigation/HomeFragment.kt
@@ -1,6 +1,42 @@
 package com.renancorredato.appcomnavigation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.renancorredato.appcomnavigation.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSignIn.setOnClickListener {
+            signIn()
+        }
+
+        binding.btnRegister.setOnClickListener {
+            register()
+        }
+    }
+
+    private fun register() {
+        findNavController().navigate(R.id.action_homeFragment_to_registerFragment)
+    }
+
+    private fun signIn() {
+        findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
+    }
 }
--- app/src/main/java/com/renancorredato/appcomnavigation/LoginFragment.kt
+++ app/src/main/java/com/renancorredato/appcomnavigation/LoginFragment.kt
@@ -1,6 +1,36 @@
 package com.renancorredato.appcomnavigation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.renancorredato.appcomnavigation.databinding.FragmentLoginBinding
 
 
-class LoginFragment : Fragment(R.layout.fragment_login)
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnBack.setOnClickListener {
+            goBack()
+        }
+    }
+
+    private fun goBack() {
+        findNavController().popBackStack()
+    }
+
+}
