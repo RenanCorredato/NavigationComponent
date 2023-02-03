16e618b4f981c02b2f49fadb46a54d662eada7e5
Renan Corredato <renancorredato@hotmail.com>
2023-02-03 12:28:41 -0300

    config navigation component

---
+++ app/src/main/java/com/renancorredato/appcomnavigation/LoginFragment.kt
@@ -0,0 +1,6 @@
+package com.renancorredato.appcomnavigation
+
+import androidx.fragment.app.Fragment
+
+
+class LoginFragment : Fragment(R.layout.fragment_login)
---
+++ app/src/main/java/com/renancorredato/appcomnavigation/RegisterFragment.kt
@@ -0,0 +1,5 @@
+package com.renancorredato.appcomnavigation
+
+import androidx.fragment.app.Fragment
+
+class RegisterFragment : Fragment(R.layout.fragment_register)
